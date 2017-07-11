d195c9987ef540e33bda453b518d707013dfd8ca
Chris Hunter <orkem2@gmail.com>
2017-07-11 11:06:29 +0100

    intial commit

---
+++ models/pizza_order.rb
@@ -0,0 +1,37 @@
+#create pizza order class fname, lname, quantity, topping id
+#save, update deleteall, delete, all
+
+class PizzaOrder
+
+
+
+
+
+
+
+
+
+
+  def save
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  def delete_all
+
+  end
+
+  def all
+
+  end
+
+
+
+end
