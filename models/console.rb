27a5325b6a83e731532bdfca666a0f47c5d38040
Chris Hunter <orkem2@gmail.com>
2017-07-11 15:19:24 +0100

    Added save function which is working

---
+++ models/console.rb
@@ -0,0 +1,8 @@
+require_relative('pizza_order.rb')
+require('pry')
+
+order1 = PizzaOrder.new({first_name: "Chris", last_name: "Hunter", topping: "Cheese", quantity: 1})
+order2 = PizzaOrder.new({first_name: "Stephen", last_name: "Beattie", topping: "Fromage", quantity: 1})
+
+order1.save
+order2.save
--- models/pizza_order.rb
+++ models/pizza_order.rb
@@ -1,19 +1,26 @@
+require('pg')
+
 #create pizza order class fname, lname, quantity, topping id
 #save, update deleteall, delete, all
 
 class PizzaOrder
 
 
-
-
-
-
-
-
-
+  def initialize(options)
+    @id = options[:id].to_i if options[:id]
+    @first_name = options[:first_name]
+    @last_name = options[:last_name]
+    @topping = options[:topping]
+    @quantity = options[:quantity]
+    @db = PG.connect(dbname: 'pizza_orders', host: 'localhost')
+  end
 
   def save
+    db = PG.connect(dbname: 'pizza_orders', host: 'localhost')
 
+    sql = "INSERT INTO pizza_orders (first_name, last_name, topping, quantity) VALUES ('#{@first_name}', '#{@last_name}', '#{@topping}', #{@quantity}) RETURNING id;"
+    @id = db.exec_params(sql)[0]['id']
+    db.close
   end
 
   def update
